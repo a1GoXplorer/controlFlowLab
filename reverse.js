f9e39d70ca4d575fd7f2a5125e384abfc7828156
a1GoXplorer <Mark_Daniels1982@hotmail.com>
2014-11-18 18:44:01 -0800

    add reverse

---
+++ reverse.js
@@ -0,0 +1,26 @@
+
+
+var inputString = "building";
+
+var FirstReverse = function(str) { 
+  var o = '';
+  for(var i = str.length - 1; i >= 0; i--)
+  {
+    o += str[i];
+ }  
+   return o;
+  // console.log(inputString);
+  };       
+
+
+console.log(FirstReverse(inputString))git;
+
+
+
+// var anotherReverse = function(str)
+// {
+// console.log(str.split("").reverse().join(""));
+// }
+
+///anotherReverse(inputString);
+
